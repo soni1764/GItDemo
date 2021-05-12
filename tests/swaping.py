5aa75c2a869fc3be3af8f4b4045e7b1da1eed80e
Soni1764 <soni.gourav1764@gmail.com>
2021-05-13 01:16:48 +0530

    This is done by Delhi Guy via master branch

--- tests/swaping.py
+++ tests/swaping.py
@@ -9,13 +9,3 @@ b = int(input("Enter first number\n"))
 result = swap(a,b)
 print(result)
 
-print("--------------------------")
-
-
-a = int(input("Enter first number\n"))
-b = int(input("Enter first number\n"))
-
-print(a, b)
-b, a = a, b
-
-print(a, b)
