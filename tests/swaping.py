c62a767a9505f40ca2625c3d0dec4db10e546792
Soni1764 <soni.gourav1764@gmail.com>
2021-05-13 00:31:59 +0530

    This is done by Banglore Guy via develop branch

--- tests/swaping.py
+++ tests/swaping.py
@@ -8,3 +8,14 @@ b = int(input("Enter first number\n"))
 
 result = swap(a,b)
 print(result)
+
+print("--------------------------")
+
+
+a = int(input("Enter first number\n"))
+b = int(input("Enter first number\n"))
+
+print(a, b)
+b, a = a, b
+
+print(a, b)
